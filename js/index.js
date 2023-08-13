eb816f609c22091f3296c37003cae29180d83a09
PedroMachado0 <pedro.rosa.denisson@gmail.com>
2023-08-13 18:24:17 -0300

    botoes

---
+++ js/index.js
@@ -0,0 +1,3 @@
+function carConteudo(){
+    document.getElementById("conteudo").innerHTML = "<p> A internet teve suas origens na década de 1960 como uma rede de computadores do Departamento de Defesa dos EUA,chamada ARPANET(Advanced Research Projects Agency Network). Originalmente,a ARPANET foi criada para permitir que cientistas eacadêmicos de diferentes locais físicos compartilhassem informações e colaborassem em pesquisa.<p>";
+}
